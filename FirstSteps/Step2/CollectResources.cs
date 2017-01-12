120f04a204d10b6bcd0685e00ae5064368178f03
LittleKolio_PC\LittleKolio <kulio_kolef@abv.bg>
2017-01-12 16:49:56 +0200

    Adding Step2

---
+++ FirstSteps/Step2/CollectResources.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step2
+{
+    class CollectResources
+    {
+        private static ISet<string> resources = new HashSet<string>()
+        {
+            "stone", "gold", "wood", "food"
+        };
+        private static bool[] visited;
+        private static string[] collection;
+
+        static void Main()
+        {
+            collection = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            int quantity = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                visited = new bool[collection.Length];
+                string[] path = Console.ReadLine().Split();
+                int start = int.Parse(path[0]);
+                int end = int.Parse(path[1]);
+            }
+        }
+    }
+}
---
+++ SomeCoding/Simple/Factorial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple
+{
+    class Factorial
+    {
+        static void Main()
+        {
+            #region n!
+            /*
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            decimal factorial = 1;
+            while (true)
+            {
+                if (n <= 1)
+                {
+                    break;
+                }
+                factorial *= n;
+                n--;
+            }
+            Console.WriteLine("n! = " + factorial);
+            */
+            #endregion
+
+            #region [n...m]!
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("m = ");
+            int m = int.Parse(Console.ReadLine());
+
+            int numSmall = Math.Min(n, m);
+            int numBig = Math.Max(n, m);
+
+            int numChange = numSmall;
+            long product = 1;
+
+            while (true)
+            {
+                if (numChange > numBig)
+                {
+                    break;
+                }
+                product *= numChange;
+                numChange++;
+            }
+            
+
+            Console.WriteLine("product[{0}...{1}] = {2}", numSmall, numBig, product);
+            #endregion
+        }
+    }
+}
