69173a5682137d22821293791162333a22f2893d
kolio-PC\kolio <kulio_kolef@bav.bg>
2017-01-15 15:57:33 +0200

    Changes to step2

--- FirstSteps/Step2/CollectResources.cs
+++ FirstSteps/Step2/CollectResources.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Step2
 {
@@ -27,7 +24,34 @@ namespace Step2
                 string[] path = Console.ReadLine().Split();
                 int start = int.Parse(path[0]);
                 int end = int.Parse(path[1]);
+
+                int quan = TryGetResource(start);
+                int currIndex = (start + end) % collection.Length;
+
+                while (!visited[currIndex])
+                {
+                    quan += TryGetResource(currIndex);
+                    currIndex = (currIndex + end) % collection.Length;
+                }
+
+                if (quan > quantity)
+                {
+                    quantity = quan;
+                }
             }
+
+            Console.WriteLine(quantity);
+        }
+
+        private static int TryGetResource(int index)
+        {
+            string[] res = collection[index].Split('_');
+            if (resources.Contains(res[0]))
+            {
+                visited[index] = true;
+                return res.Length > 1 ? int.Parse(res[1]) : 1;
+            }
+            return 0;
         }
     }
 }
