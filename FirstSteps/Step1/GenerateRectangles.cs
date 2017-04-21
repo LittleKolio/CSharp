aef2b53445228b07375c72edf632d0e3e8e51b00
LittleKolio_PC\LittleKolio <kulio_kolef@abv.bg>
2017-04-21 13:21:22 +0300

    Change folder step1 to Step1

---
+++ FirstSteps/Step1/GenerateRectangles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step1
+{
+    class GenerateRectangles
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int area = int.Parse(Console.ReadLine());
+
+            if (Math.Sqrt(num) < area)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ FirstSteps/Step1/RectangleWithStars.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step1
+{
+    class RectangleWithStars
+    {
+        static void Main ()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int rowsIn = n % 2 == 0 ? (n - 1) : n;
+
+            Console.WriteLine(new string('%', n * 2));
+
+            for (int i = 1; i <= rowsIn; i++)
+            {
+                string row = "%";
+                if (i == rowsIn / 2 + 1)
+                {
+                    row += new string(' ', n - 2);
+                    row += "**";
+                    row += new string(' ', n - 2);
+                }
+                else
+                {
+                    row += new string(' ', n * 2 - 2);
+                }
+                row += "%";
+
+                Console.WriteLine(row);
+                
+            }
+
+            Console.WriteLine(new string('%', n * 2));
+        }
+    }
+}
