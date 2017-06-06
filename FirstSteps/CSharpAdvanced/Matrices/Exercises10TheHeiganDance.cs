a42e7d77294f02294a28c151aafdc910f7336407
LittleKolio_PC\LittleKolio <kulio_kolef@abv.bg>
2017-06-07 00:53:07 +0300

    Manual Strings

---
+++ FirstSteps/CSharpAdvanced/Matrices/Exercises10TheHeiganDance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced.Matrices
+{
+    class Exercises10TheHeiganDance
+    {
+        const int Num = 15;
+        const int Cloud = 3500;
+        const int Eruption = 6000;
+        const int PlayerHealth = 18500;
+        const int HeiganHealth = 3000000;
+
+        static void Main()
+        {
+            double damagePerTurn = double.Parse(Console.ReadLine());
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
