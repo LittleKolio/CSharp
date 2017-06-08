d8b2c616eb8d4aff265041402a4e8fba270ca70c
kolio-PC\kolio <kulio_kolef@bav.bg>
2017-06-08 20:11:16 +0300

    Just one exercise

---
+++ FirstSteps/CSharpAdvanced/String/Exercises02StringLength.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced.String
+{
+    class Exercises02StringLength
+    {
+        static void Main()
+        {
+            int length = 20;
+
+            string text = Console.ReadLine();
+
+            if (text.Length >= length)
+            {
+                Console.WriteLine(text.Substring(0, length));
+            }
+            else
+            {
+                Console.WriteLine(text.PadRight(20, '*'));
+            }
+        }
+    }
+}
---
+++ FirstSteps/CSharpAdvanced/String/Exercises03FormattingNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced.String
+{
+    class Exercises03FormattingNumbers
+    {
+        static void Main()
+        {
+            double[] num = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+
+            string format = "{0,10:X}|{0}";
+            string textLine = string.Format(format, (int)num[0], Convert.ToString((int)num[0], 8));
+
+            Console.WriteLine(textLine);
+        }
+    }
+}
