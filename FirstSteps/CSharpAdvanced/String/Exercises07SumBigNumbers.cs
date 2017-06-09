3a4fb02030706e6e4294551f949ee7ae23d5c7af
kolio-PC\kolio <kulio_kolef@bav.bg>
2017-06-09 19:52:18 +0300

    Still manual strings exercises

---
+++ FirstSteps/CSharpAdvanced/String/Exercises07SumBigNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced.String
+{
+    class Exercises07SumBigNumbers
+    {
+        static void Main()
+        {
+            Stack<char> num1 = new Stack<char>(Console.ReadLine());
+            Stack<char> num2 = new Stack<char>(Console.ReadLine());
+
+            while (num1.Count > 0)
+            {
+                Console.Write(num1.Pop());
+            }
+        }
+    }
+}
