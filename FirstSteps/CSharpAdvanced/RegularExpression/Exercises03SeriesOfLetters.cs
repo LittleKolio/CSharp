837a8fe44d34840785b0cfc3528462301aa5bbad
kolio-PC\kolio <kulio_kolef@bav.bg>
2017-06-11 15:51:03 +0300

    Regular expression exercises

---
+++ FirstSteps/CSharpAdvanced/RegularExpression/Exercises03SeriesOfLetters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced.RegularExpression
+{
+    class Exercises03SeriesOfLetters
+    {
+        private const string pattern = "(\\w)\\1+";
+
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            string result = Regex.Replace(input, pattern, m => m.Groups[1].Value);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ FirstSteps/CSharpAdvanced/RegularExpression/Exercises04ReplaceTag.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced.RegularExpression
+{
+    class Exercises04ReplaceTag
+    {
+        private const string pattern = 
+            "<a\\s*href=('|\")(?<href>.+)\\1>(?<text>\\w+)<\\/a>";
+        private const string format = "[URL href=\"{0}\"]{1}[/URL]";
+
+        static void Main()
+        {
+            Regex regex = new Regex(pattern);
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "end") { break; }
+
+                string tag = Regex.Replace(
+                    input, 
+                    pattern, 
+                    m => string.Format(
+                        format, 
+                        m.Groups["href"].Value, 
+                        m.Groups["text"].Value));
+
+                Console.WriteLine(tag);
+            }
+        }
+    }
+}
