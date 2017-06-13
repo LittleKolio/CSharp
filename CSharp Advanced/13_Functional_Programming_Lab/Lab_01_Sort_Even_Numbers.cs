099263243e91e2ca324624cfec635692a2dc54dc
LittleKolio_PC\LittleKolio <kulio_kolef@abv.bg>
2017-06-13 23:09:20 +0300

    Functional programming lab

---
+++ CSharp Advanced/13_Functional_Programming_Lab/Lab_01_Sort_Even_Numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming_Lab
+{
+    class Lab_01_Sort_Even_Numbers
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine()
+                .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
