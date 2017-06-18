5732dfc3461853ce7b95f4d6037975bea12022c2
LittleKolio_PC\LittleKolio <kulio_kolef@abv.bg>
2017-06-19 00:46:33 +0300

    LINQ exercises

--- CSharp Advanced/16_LINQ_Exercises/Exercises_10_Group_By_Group.cs
+++ CSharp Advanced/16_LINQ_Exercises/Exercises_10_Group_By_Group.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace LINQ_Exercises
 {
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Group { get; set; }
+    }
     class Exercises_10_Group_By_Group
     {
+        static void Main()
+        {
+            List<Person> students = new List<Person>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(new[] { ' ' },
+                        StringSplitOptions.RemoveEmptyEntries);
+                if (input[0].ToLower() == "end") { break; }
+
+                students.Add(new Person
+                {
+                    Name = string.Join(" ", input.Take(2)),
+                    Group = int.Parse(input[2])
+                });
+            }
+
+            students
+                .GroupBy(stu => stu.Group)
+                .OrderBy(group => group.Key)
+                .Select(group => new
+                {
+                    Group = group.Key,
+                    Students = group
+                        .Select(stu => stu.Name)
+                        .ToList()
+                })
+                .ToList()
+                .ForEach(obj => 
+                    Console.WriteLine(
+                        "{0} - {1}", 
+                        obj.Group, 
+                        string.Join(", ", obj.Students)));
+
+        }
     }
 }
---
+++ CSharp Advanced/16_LINQ_Exercises/Exercises_11_Students_Joined_To_Specialties.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Exercises
+{
+    public class Speciality
+    {
+        public string Name { get; set; }
+        public int FacultyNumber { get; set; }
+    }
+    public class Student
+    {
+        public string Name { get; set; }
+        public int FacultyNumber { get; set; }
+    }
+    class Exercises_11_Students_Joined_To_Specialties
+    {
+        static void Main()
+        {
+            List<Speciality> specialities = new List<Speciality>();
+            List<Student> students = new List<Student>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(new[] { ' ' },
+                        StringSplitOptions.RemoveEmptyEntries);
+                if (input[0].ToLower() == "students:") { break; }
+
+                specialities.Add(new Speciality
+                {
+                    Name = string.Join(" ", input.Take(2)),
+                    FacultyNumber = int.Parse(input[2])
+                });
+            }
+
+            while (true)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(new[] { ' ' },
+                        StringSplitOptions.RemoveEmptyEntries);
+                if (input[0].ToLower() == "end") { break; }
+
+                students.Add(new Student
+                {
+                    Name = string.Join(" ", input.Skip(1)),
+                    FacultyNumber = int.Parse(input[0])
+                });
+            }
+
+            students
+                .OrderBy(stu => stu.Name)
+                .Join(
+                    specialities, 
+                    stu => stu.FacultyNumber, 
+                    spe => spe.FacultyNumber,
+                    (stu, spe) => new
+                        {
+                            Name = stu.Name,
+                            FacNum = stu.FacultyNumber,
+                            Spec = spe.Name
+                        })
+                .ToList()
+                .ForEach(stu => 
+                    Console.WriteLine(
+                        $"{stu.Name} {stu.FacNum} {stu.Spec}"));
+        }
+    }
+}
