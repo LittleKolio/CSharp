7dfd85098acc79f367c7464a5e21048480749108
kolio-PC\kolio <kulio_kolef@bav.bg>
2017-06-18 10:31:39 +0300

    LINQ exercises

--- CSharp Advanced/15_LINQ_Lab/Lab_08_Map_Districts.cs
+++ CSharp Advanced/15_LINQ_Lab/Lab_08_Map_Districts.cs
@@ -10,12 +10,39 @@ namespace LINQ_Lab
     {
         static void Main()
         {
-            var input = Console.ReadLine()
+            //List<KeyValuePair<string, int>> input = Console.ReadLine()
+            //    .Split(new[] { ' ' },
+            //        StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(str => new KeyValuePair<string, int>(
+            //        str.Split(':')[0],
+            //        int.Parse(str.Split(':')[1])))
+            //    .ToList();
+
+            string cities = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+
+            var result = cities
                 .Split(new[] { ' ' },
                     StringSplitOptions.RemoveEmptyEntries)
+                .GroupBy(
+                    str => str.Split(':')[0],
+                    str => int.Parse(str.Split(':')[1]))
+                .Select(group => new KeyValuePair<string, int[]>(
+                    group.Key,
+                    group.ToArray()))
+                .Where(pair => pair.Value.Sum() > population)
+                .OrderByDescending(pair => pair.Value.Sum())
                 .ToDictionary(
-                    str => str.Split(':')[0], 
-                    str => int.Parse(str.Split(':')[1]));
+                    pair => pair.Key,
+                    pair => pair.Value
+                        .OrderByDescending(pop => pop)
+                        .Take(5)
+                        .ToArray());
+
+            foreach (var city in result)
+            {
+                Console.WriteLine($"{city.Key}: " + string.Join(" ", city.Value));
+            }
         }
     }
 }
---
+++ CSharp Advanced/16_LINQ_Exercises/Exercises_01_Students_By_Group.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Exercises
+{
+    class Exercises_01_Students_By_Group
+    {
+    }
+}
