bef28e7542a3282c002a71c45ad89f82691340fd
kolio-PC\kolio <kulio_kolef@bav.bg>
2017-06-18 12:34:56 +0300

    LINQ lab 8 GroupBy

--- CSharp Advanced/15_LINQ_Lab/Lab_08_Map_Districts.cs
+++ CSharp Advanced/15_LINQ_Lab/Lab_08_Map_Districts.cs
@@ -6,21 +6,29 @@ using System.Threading.Tasks;
 
 namespace LINQ_Lab
 {
+    public class City
+    {
+        public City()
+        {
+            District = new List<long>();
+        }
+        public string Name { get; set; }
+        public List<long> District { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}: " + string.Join(" ", District);
+        }
+    }
+
     class Lab_08_Map_Districts
     {
         static void Main()
         {
-            //List<KeyValuePair<string, int>> input = Console.ReadLine()
-            //    .Split(new[] { ' ' },
-            //        StringSplitOptions.RemoveEmptyEntries)
-            //    .Select(str => new KeyValuePair<string, int>(
-            //        str.Split(':')[0],
-            //        int.Parse(str.Split(':')[1])))
-            //    .ToList();
-
             string cities = Console.ReadLine();
-            int population = int.Parse(Console.ReadLine());
+            long populationLimit = long.Parse(Console.ReadLine());
 
+            /*
             var result = cities
                 .Split(new[] { ' ' },
                     StringSplitOptions.RemoveEmptyEntries)
@@ -30,7 +38,7 @@ namespace LINQ_Lab
                 .Select(group => new KeyValuePair<string, int[]>(
                     group.Key,
                     group.ToArray()))
-                .Where(pair => pair.Value.Sum() > population)
+                .Where(pair => pair.Value.Sum() > populationLimit)
                 .OrderByDescending(pair => pair.Value.Sum())
                 .ToDictionary(
                     pair => pair.Key,
@@ -42,6 +50,60 @@ namespace LINQ_Lab
             foreach (var city in result)
             {
                 Console.WriteLine($"{city.Key}: " + string.Join(" ", city.Value));
+            }
+            */
+
+            /*
+            var result = cities
+                .Split(new[] { ' ' },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .Select(str => new
+                {
+                    Name = str.Split(':')[0],
+                    Population = long.Parse(str.Split(':')[1])
+                })
+                .GroupBy(obj => obj.Name)
+                .Select(group => new City
+                {
+                    Name = group.Key,
+                    District = group
+                        .Select(obj => obj.Population)
+                        .OrderByDescending(popu => popu)
+                        .Take(5)
+                        .ToList()
+                })
+                .Where(city => city.District.Sum() > populationLimit)
+                .OrderByDescending(city => city.District.Sum())
+                .ToList();
+            */
+
+            var result = cities
+                .Split(new[] { ' ' },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .Select(str => new
+                {
+                    Name = str.Split(':')[0],
+                    Population = long.Parse(str.Split(':')[1])
+                })
+                .GroupBy(
+                    obj => obj.Name,
+                    obj => obj.Population,
+                    (name, dist) => new City
+                    {
+                        Name = name,
+                        District = dist
+                            .OrderByDescending(pop => pop)
+                            .Take(5)
+                            .ToList()
+                    })
+                .Where(city => city.District.Sum() > populationLimit)
+                .OrderByDescending(city => city.District.Sum())
+                .ToList();
+
+
+            foreach (var city in result)
+            {
+                Console.WriteLine("" + city);
             }
         }
     }
