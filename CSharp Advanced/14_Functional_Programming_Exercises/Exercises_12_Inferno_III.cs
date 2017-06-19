d07641598ea937298c94d796ad6fb3abb3b8dce0
LittleKolio_PC\LittleKolio <kulio_kolef@abv.bg>
2017-06-19 23:58:59 +0300

    12 from fuctional programming and 13 from LINQ

---
+++ CSharp Advanced/14_Functional_Programming_Exercises/Exercises_12_Inferno_III.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming_Exercises
+{
+    class Exercises_12_Inferno_III
+    {
+        public static int[] gems;
+        static void Main()
+        {
+            gems = Console.ReadLine()
+                .Split(new[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Dictionary<string, Dictionary<int, Predicate<int>>> commands
+                = new Dictionary<string, Dictionary<int, Predicate<int>>>();
+
+            while (true)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(new[] { ';' },
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens[0].ToLower() == "forge") { break; }
+
+                string command = tokens[0].ToLower();
+                string filter = tokens[1];
+                int param = int.Parse(tokens[2]);
+
+                Predicate<int> func = SwitchFunction(filter, param);
+
+                if (command == "exclude")
+                {
+                    if (!commands.ContainsKey(filter))
+                    {
+                        commands.Add(filter,
+                            new Dictionary<int, Predicate<int>>());
+                    }
+
+                    commands[filter].Add(param, func);
+                }
+                else
+                {
+                    commands[filter].Remove(param);
+                }
+            }
+
+            List<int> result = Forge(commands);
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static List<int> Forge(Dictionary<string, Dictionary<int, Predicate<int>>> commands)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < gems.Length; i++)
+            {
+                bool validIndex = false;
+                foreach (var command in commands)
+                {
+                    foreach (var filter in command.Value)
+                    {
+                        if (filter.Value(i))
+                        {
+                            validIndex = true;
+                            break;
+                        }
+                    }
+                }
+                if (!validIndex)
+                {
+                    result.Add(gems[i]);
+                }
+            }
+
+            return result;
+        }
+
+        private static Predicate<int> SwitchFunction(string filter, int param)
+        {
+            switch (filter)
+            {
+                case "Sum Left":
+                    return index =>
+                    {
+                        int gem = index <= 0 
+                            ? 0 
+                            : gems[index - 1];
+                        return (gem + gems[index]) == param;
+                    };
+
+                case "Sum Right":
+                    return index =>
+                    {
+                        int gem = index >= gems.Length - 1 
+                            ? 0 
+                            : gems[index + 1];
+                        return (gem + gems[index]) == param;
+                    };
+
+                case "Sum Left Right":
+                    return index =>
+                    {
+                        int gemRight = index >= gems.Length - 1
+                            ? 0
+                            : gems[index + 1];
+                        int gemLeft = index <= 0
+                            ? 0
+                            : gems[index - 1];
+                        return (gemRight + gemLeft + gems[index]) == param;
+                    };
+
+                default: return null;
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/16_LINQ_Exercises/Exercises_13_Office_Stuff.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Exercises
+{
+    public class Stuff
+    {
+        public string company;
+        public int amount;
+        public string product;
+        public Stuff(string company, int amount, string product)
+        {
+            this.company = company;
+            this.amount = amount;
+            this.product = product;
+        }
+    }
+    class Exercises_13_Office_Stuff
+    {
+        static void Main()
+        {
+            int order = int.Parse(Console.ReadLine());
+
+            List<Stuff> comapnyProducts = new List<Stuff>();
+
+            for (int i = 0; i < order; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split("|- ".ToCharArray(),
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                comapnyProducts.Add(new Stuff(
+                    input[0], int.Parse(input[1]), input[2]
+                    ));
+            }
+
+            var result = comapnyProducts
+                .GroupBy(stuff => stuff.company)
+                .OrderBy(gCompany => gCompany.Key)
+                .Select(gCompany => new
+                {
+                    Company = gCompany.Key,
+                    Products = gCompany
+                        .GroupBy(stuff => stuff.product)
+                        .Select(gProduct =>
+                            gProduct.Key + "-" +
+                            gProduct.Select(pNum => pNum.amount).Sum())
+                });
+
+            foreach (var comapny in result)
+            {
+                Console.Write(comapny.Company + ": ");
+                Console.WriteLine(string.Join(", ", comapny.Products));
+            }
+        }
+    }
+}
