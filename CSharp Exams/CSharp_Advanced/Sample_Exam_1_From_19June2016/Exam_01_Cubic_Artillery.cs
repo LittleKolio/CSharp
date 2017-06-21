f9c63366a6db8e27ba0e6dcf3c042c89e6e0e0da
kolio-PC\kolio <kulio_kolef@bav.bg>
2017-06-21 19:43:56 +0300

    Change work place

--- CSharp Exams/CSharp_Advanced/Sample_Exam_1_From_19June2016/Exam_01_Cubic_Artillery.cs
+++ CSharp Exams/CSharp_Advanced/Sample_Exam_1_From_19June2016/Exam_01_Cubic_Artillery.cs
@@ -8,8 +8,14 @@ namespace Sample_Exam_1_From_19June2016
 {
     class Exam_01_Cubic_Artillery
     {
+        public static Queue<KeyValuePair<string, List<int>>> bunkers;
+        public static Queue<int> weapons;
+
         static void Main()
         {
+            bunkers = new Queue<KeyValuePair<string, List<int>>>();
+            weapons = new Queue<int>();
+
             int capacity = int.Parse(Console.ReadLine());
 
             while (true)
@@ -19,26 +25,53 @@ namespace Sample_Exam_1_From_19June2016
                         StringSplitOptions.RemoveEmptyEntries);
                 if (input[0] == "Bunker") { break; }
 
-                Queue<KeyValuePair<string, List<int>>> bunkers
-                    = new Queue<KeyValuePair<string, List<int>>>();
-                Queue<int> weapons = new Queue<int>();
+                BunkersAndWeapons(input);
 
-                BunkersAndWeapons(input, ref bunkers, ref weapons);
-
-                int tempBCount = bunkers.Count;
-
-                for (int j = 1;
-                    j <= tempBCount || bunkers.Count > 1;
-                    j++)
+                if (bunkers.Count == 1)
                 {
-                    KeyValuePair<string, List<int>> tempBunker 
-                        = bunkers.Dequeue();
-                    bool remove = false;
-                    int tempWCount = weapons.Count;
+                    KeyValuePair<string, List<int>> tempBunker = bunkers.Dequeue();
+                    int freeSpace = capacity - tempBunker.Value.Sum();
 
-                    for (int i = 1; 
-                        i <= tempWCount || weapons.Count > 1;
-                        i++)
+                    for (int i = 1; weapons.Count > 1; i++)
+                    {
+                        int tempWeapon = weapons.Dequeue();
+
+                        if (freeSpace >= tempWeapon)
+                        {
+                            tempBunker.Value.Add(tempWeapon);
+                            if (tempBunker.Value.Sum() == capacity)
+                            {
+                                Console.WriteLine($"{tempBunker.Key} -> " + string.Join(", ", tempBunker.Value));
+                            }
+                        }
+                        else if (capacity >= tempWeapon)
+                        {
+                            List<int> newList = new List<int>();
+                            int tempSum = freeSpace;
+                            int count = tempBunker.Value.Count;
+                            for (int k = 0; k < count; k++)
+                            {
+                                tempSum += tempBunker.Value[k];
+                                if (tempSum >= tempWeapon)
+                                {
+                                    newList.AddRange(weapons.Skip(k + 1));
+                                    newList.Add(tempWeapon);
+                                }
+                            }
+                            if (newList.Count > 0)
+                            {
+
+                            }
+                        }
+                    }
+                }
+
+                for (int j = 1; bunkers.Count > 1; j++)
+                {
+                    KeyValuePair<string, List<int>> tempBunker = bunkers.Dequeue();
+                    bool remove = false;
+
+                    for (int i = 1; weapons.Count > 1; i++)
                     {
                         int tempWeapon = weapons.Dequeue();
 
@@ -47,9 +80,13 @@ namespace Sample_Exam_1_From_19June2016
                             tempBunker.Value.Add(tempWeapon);
                             if (tempBunker.Value.Sum() == capacity)
                             {
-                                Console.WriteLine($"{tempBunker.Key} -> " + string.Join(", ", b.Value));
+                                Console.WriteLine($"{tempBunker.Key} -> " + string.Join(", ", tempBunker.Value));
                                 remove = true;
                             }
+                        }
+                        else
+                        {
+                            weapons.Enqueue(tempWeapon);
                         }
                     }
 
@@ -68,10 +105,7 @@ namespace Sample_Exam_1_From_19June2016
             }
         }
 
-        private static void BunkersAndWeapons(
-            string[] input, 
-            ref Queue<KeyValuePair<string, List<int>>> bunkers, 
-            ref Queue<int> weapons)
+        private static void BunkersAndWeapons(string[] input)
         {
             string bunker;
             for (int i = 0; i < input.Length; i++)
