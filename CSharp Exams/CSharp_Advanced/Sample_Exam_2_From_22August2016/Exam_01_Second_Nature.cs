0e611dbf657993e118aba17624b5a94bcee35eac
LittleKolio_PC\LittleKolio <kulio_kolef@abv.bg>
2017-06-24 00:29:56 +0300

    Sample exam 2

--- CSharp Exams/CSharp_Advanced/Sample_Exam_2_From_22August2016/Exam_01_Second_Nature.cs
+++ CSharp Exams/CSharp_Advanced/Sample_Exam_2_From_22August2016/Exam_01_Second_Nature.cs
@@ -8,5 +8,70 @@ namespace Sample_Exam_2_From_22August2016
 {
     class Exam_01_Second_Nature
     {
+        static void Main()
+        {
+            Stack<int> flowers = new Stack<int>(
+                Console.ReadLine()
+                .Split(new[] { ' ' },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                );
+
+            Stack<int> water = new Stack<int>(
+                Console.ReadLine()
+                .Split(new[] { ' ' },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                );
+
+            Queue<int> list = new Queue<int>();
+
+            while (flowers.Count > 0 && water.Count > 0)
+            {
+                int tempW = water.Pop();
+                int tempF = flowers.Pop();
+
+                int result = tempF - tempW;
+
+                if (result == 0)
+                {
+                    list.Enqueue(tempF);
+                }
+                else if (result < 0)
+                {
+                    if (water.Count > 0)
+                    {
+                        water.Push(water.Pop() + Math.Abs(result));
+                    }
+                    else
+                    {
+                        water.Push(result);
+                    }
+                }
+                else if (result > 0)
+                {
+                    flowers.Push(result);
+                }
+            }
+
+            if (water.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", water));
+            }
+            else if (flowers.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", flowers));
+            }
+
+            if (list.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", list));
+            }
+            else
+            {
+                Console.WriteLine("None");
+            }
+        }
     }
 }
---
+++ CSharp Exams/CSharp_Advanced/Sample_Exam_2_From_22August2016/Exam_02_Natures_Prophet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Exam_2_From_22August2016
+{
+    class Exam_02_Natures_Prophet
+    {
+        static void Main()
+        {
+            int[] dimencions = Console.ReadLine()
+                .Split(new[] { ' ' },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[][] matrix = new int[dimencions[0]][];
+            for (int row = 0; row < dimencions[0]; row++)
+            {
+                matrix[row] = new int[dimencions[1]];
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Bloom Bloom Plow") { break; }
+
+                int[] position = input
+                .Split(new[] { ' ' },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+
+            }
+        }
+    }
+}
