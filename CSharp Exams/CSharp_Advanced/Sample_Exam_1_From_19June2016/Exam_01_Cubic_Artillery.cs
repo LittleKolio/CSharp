b86cb4b1e7a7ccf7303e0bfbed9b31cf3dc53270
LittleKolio_PC\LittleKolio <kulio_kolef@abv.bg>
2017-06-24 20:13:48 +0300

    Reworking sample exam 01

--- CSharp Exams/CSharp_Advanced/Sample_Exam_1_From_19June2016/Exam_01_Cubic_Artillery.cs
+++ CSharp Exams/CSharp_Advanced/Sample_Exam_1_From_19June2016/Exam_01_Cubic_Artillery.cs
@@ -8,117 +8,103 @@ namespace Sample_Exam_1_From_19June2016
 {
     /// <summary>
     /// You will be given n - an integer specifying the bunker's maximum capacity.
-    /// Then you will be given input lines which will contain English alphabet letters and numbers, separated by a single space.
+    /// Then you will be given input lines which will contain English alphabet letters and numbers, 
+    /// separated by a single space.
     /// The letters represent the bunkers and the numbers - the weapons and their capacity.
     /// Weapons must be stored in the bunkers.
-    /// 
+    /// When a bunker cannot contain a given weapon due to lack of enough free capacity it overflows, 
+    /// and passes the weapon to the next entered bunker, then to the next, 
+    /// and so on till the last entered bunker.
+    /// If there is no bunker that can hold the given weapon, ignore that weapon.
+    /// If there are no other bunkers besides the current one, 
+    /// you must check if current weapon can actually be contained.
+    /// If the weapon can be contained, you must make enough free capacity to hold that weapon, 
+    /// by removing weapons, starting from the first entered, till the last.
+    /// If a bunker overflows you must remove it, and print it on the console.
+    /// If there are no other bunkers, you must NOT remove the one that overflowed.
     /// </summary>
+    /// <output>
+    /// You must print a bunker, every time it overflows, after removing it.
+    /// The format is the following: {bunker} -> {weapon1}, {weapossn2}
+    /// In case a bunker has no weapons, just print "Empty".
+    /// </output>
+    /// <remarks>
+    /// The bunkers will only be English alphabet letters.
+    /// Each bunker's letter will always be unique.
+    /// The integer n will be In the range [0, 500].
+    /// The weapons will always be valid integers in the range [0, 500].
+    /// Allowed time/memory: 100ms/16MB.
+    /// </remarks>
     class Exam_01_Cubic_Artillery
     {
-        public static Queue<string> bunkers;
-        public static Queue<int> weapons;
-
         static void Main()
         {
-            bunkers = new Queue<string>();
-            weapons = new Queue<int>();
-
             int capacity = int.Parse(Console.ReadLine());
 
-            while (true)
+            Queue<string> bunkers = new Queue<string>();
+            Queue<int> weapons = new Queue<int>();
+            
+
+            string input;
+            while ((input = Console.ReadLine()) != "Bunker Revision")
             {
-                string[] input = Console.ReadLine()
+                string[] formatInput = input
                     .Split(new[] { ' ' },
                         StringSplitOptions.RemoveEmptyEntries);
-                if (input[0] == "Bunker") { break; }
 
-                BunkersAndWeapons(input, capacity);
-
-                string tempBunker;
-                Queue<int> tempWeapons;
-                int freeSpace;
-
-                while (bunkers.Count > 1)
+                foreach (string item in formatInput)
                 {
-                    freeSpace = capacity;
-                    tempWeapons = new Queue<int>();
-                    tempBunker = bunkers.Dequeue();
-
-                    for (int j = 1; j < weapons.Count; j++)
+                    int weapon;
+                    bool isWeapon = int.TryParse(item, out weapon);
+                    if (!isWeapon)
                     {
-                        int tempWeapon = weapons.Dequeue();
-                        if (freeSpace >= tempWeapon)
+                        bunkers.Enqueue(item);
+                    }
+                    else if (weapon <= capacity)
+                    {
+                        weapons.Enqueue(weapon);
+                    }
+                }
+
+                int freeSpace = capacity;
+                List<int> currentWs = new List<int>();
+
+                while (weapons.Count > 0)
+                {
+                    int currentW = weapons.Dequeue();
+                    bool saved = false;
+                    while (bunkers.Count > 1)
+                    {
+                        if (freeSpace >= currentW)
                         {
-                            tempWeapons.Enqueue(tempWeapon);
-                            freeSpace -= tempWeapon;
-                            j--;
+                            currentWs.Add(currentW);
+                            freeSpace -= currentW;
+                            saved = true;
+                            break;
                         }
                         else
                         {
-                            weapons.Enqueue(tempWeapon);
+                            Console.WriteLine($"{bunkers.Dequeue()} -> {string.Join(", ", currentWs)}");
                         }
+                        freeSpace = capacity;
+                        currentWs.Clear();
                     }
 
-                    Console.WriteLine($"{tempBunker} -> " +
-                        string.Join(", ", tempWeapons));
+                    if (!false)
+                    {
+                        if (freeSpace >= currentW)
+                        {
+                            currentWs.Add(currentW);
+                            freeSpace -= currentW;
+                        }
+
+                    }
                 }
 
-                freeSpace = capacity;
-                tempWeapons = new Queue<int>();
-                tempBunker = bunkers.Dequeue();
-
-                if (weapons.Count == 0)
+                if (weapons.Count == 0 && bunkers.Count > 0)
                 {
-                    Console.WriteLine($"{tempBunker} -> Empty");
+                    Console.WriteLine($"{bunkers.Dequeue()} -> Empty");
                     break;
-                }
-                else
-                {
-                    while (weapons.Count > 0)
-                    {
-                        int tempWeapon = weapons.Dequeue();
-
-                        if (freeSpace >= tempWeapon)
-                        {
-                            tempWeapons.Enqueue(tempWeapon);
-                            freeSpace -= tempWeapon;
-                        }
-                        else
-                        {
-                            while (tempWeapons.Count > 0)
-                            {
-                                freeSpace += tempWeapons.Dequeue();
-                                if (freeSpace >= tempWeapon)
-                                {
-                                    tempWeapons.Enqueue(tempWeapon);
-                                    freeSpace -= tempWeapon;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-
-                    Console.WriteLine($"{tempBunker} -> " +
-                        string.Join(", ", tempWeapons));
-                }
-            }
-        }
-
-        private static void BunkersAndWeapons(string[] input, int capacity)
-        {
-            string bunker;
-            foreach (string item in input)
-            {
-                int weapon;
-                bool isWeapon = int.TryParse(item, out weapon);
-                if (!isWeapon)
-                {
-                    bunker = item;
-                    bunkers.Enqueue(bunker);
-                }
-                else if (capacity >= weapon)
-                {
-                    weapons.Enqueue(weapon);
                 }
             }
         }
