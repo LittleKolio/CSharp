d85f8dda5b699fd2a6b0f91351dfc2419195cb0b
kolio-PC\kolio <kulio_kolef@bav.bg>
2017-07-28 22:20:06 +0300

    Iterators and comparators exercises

---
+++ OOP Advanced/06_Iterators_And_Comparators_Exercises/Exercises_01_02/Exercises_02_Collection.cs
@@ -0,0 +1,52 @@
+﻿namespace Iterators_And_Comparators_Exercises
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Exercises_02_Collection
+    {
+        static void Main()
+        {
+            string[] elements = Console.ReadLine()
+                .Split(' ')
+                .Skip(1)
+                .ToArray();
+
+            ListyIterator<string> list = new ListyIterator<string>(elements);
+
+            string command;
+            while (!(command = Console.ReadLine()).Equals("END"))
+            {
+                switch (command)
+                {
+                    case "Move":
+                        Console.WriteLine(list.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(list.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            Console.WriteLine(list.Print());
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case "PrintAll":
+                        foreach (var item in list)
+                        {
+                            Console.Write(item + " ");
+                        }
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ OOP Advanced/06_Iterators_And_Comparators_Exercises/Exercises_01_02/ListyIterator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ListyIterator<T> : IEnumerable<T>
+{
+    private readonly IList<T> list;
+    private int index;
+
+    public ListyIterator(params T[] elements)
+    {
+        this.Reset();
+        this.list = new List<T>(elements);
+    }
+
+    public T Current
+    {
+        get { return this.list[this.index]; }
+    }
+
+    public bool Move()
+    {
+        bool result = HasNext();
+        if (result) { this.index++; }
+        return result;
+    }
+
+    public bool HasNext()
+    {
+        return (this.index + 1) < this.list.Count;
+    }
+
+    public void Reset()
+    {
+        this.index = 0;
+    }
+
+    public T Print()
+    {
+        if (!this.list.Any())
+        {
+            throw new InvalidOperationException(
+                "Invalid Operation!");
+        }
+        return this.Current;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < this.list.Count; i++)
+        {
+            yield return this.list[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
