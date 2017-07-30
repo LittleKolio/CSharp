a0f68c115e21368a91f096083e0310b40c20a032
kolio-PC\kolio <kulio_kolef@bav.bg>
2017-07-30 12:34:05 +0300

    Enumerations and attributes

---
+++ OOP Advanced/07_Enumerations_And_Attributes_Lab/Lab_03_04/Lab_03_Create_Attribute.cs
@@ -0,0 +1,17 @@
+﻿namespace Enumerations_And_Attributes_Lab
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [SoftUni("Ventsi")]
+    public class Lab_03_Create_Attribute
+    {
+        [SoftUni("Gosho")]
+        static void Main()
+        {
+        }
+    }
+}
