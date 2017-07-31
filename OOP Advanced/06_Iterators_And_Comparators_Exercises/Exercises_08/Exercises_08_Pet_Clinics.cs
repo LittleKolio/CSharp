43fb2fe320c18fc225b972e29ffa16bcd0eb69d9
LittleKolio_PC\LittleKolio <kulio_kolef@abv.bg>
2017-07-31 21:11:32 +0300

    Auto stash before merge of "master" and "origin/master"

---
+++ OOP Advanced/06_Iterators_And_Comparators_Exercises/Exercises_08/Exercises_08_Pet_Clinics.cs
@@ -0,0 +1,39 @@
+﻿namespace Iterators_And_Comparators_Exercises
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using System.Threading.Tasks;
+    class Exercises_08_Pet_Clinics
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                List<string> input = Console.ReadLine()
+                    .Split(new[] { ' ' },
+                        StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                string cmd = input[0];
+                input.Remove(cmd);
+
+                switch (cmd)
+                {
+                    case "Create":
+                        CreateEntity(input);
+                        break;
+                }
+            }
+        }
+
+        private static void CreateEntity(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OOP Advanced/06_Iterators_And_Comparators_Exercises/Exercises_08/Models/Clinic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Clinic
+{
+    private int roomNumber;
+
+    public Clinic(int roomNumber)
+    {
+        this.RoomNumber = roomNumber;
+    }
+
+    public int RoomNumber
+    {
+        get { return this.roomNumber; }
+        set
+        {
+            if (value % 2 == 0)
+            {
+                throw new ArgumentException(
+                    "Invalid Operation!");
+            }
+            this.roomNumber = value;
+        }
+    }
+
+}
