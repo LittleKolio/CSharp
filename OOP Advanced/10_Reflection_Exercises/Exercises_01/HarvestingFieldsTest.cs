3065c0e6fe9b057e770c01ea6ac1e909cba35537
kolio-PC\kolio <kulio_kolef@bav.bg>
2017-08-04 07:12:27 +0300

    Reflection

--- OOP Advanced/10_Reflection_Exercises/Exercises_01/HarvestingFieldsTest.cs
+++ OOP Advanced/10_Reflection_Exercises/Exercises_01/HarvestingFieldsTest.cs
@@ -1,12 +1,66 @@
 ﻿namespace Reflection_Exercises
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
 
-    class HarvestingFieldsTest
+    public class HarvestingFieldsTest
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            //TODO put your reflection code here
+            Type harvesting = typeof(HarvestingFields);
+            FieldInfo[] fields = harvesting.GetFields(
+                BindingFlags.NonPublic |
+                BindingFlags.Public |
+                BindingFlags.Instance |
+                BindingFlags.Static
+                );
+
+            Dictionary<string, Func<FieldInfo[]>> filter = new Dictionary<string, Func<FieldInfo[]>>()
+            {
+                { "private", () => fields.Where(f => f.IsPrivate).ToArray() },
+                { "protected", () => fields.Where(f => f.IsFamily).ToArray() },
+                { "public", () => fields.Where(f => f.IsPublic).ToArray() },
+                { "all", () => fields }
+            };
+
+            //FieldInfo[] filterFields;
+
+            string input;
+            while ((input = Console.ReadLine()) != "HARVEST")
+            {
+                #region switch () { }
+                /*
+                switch (input)
+                {
+                    case "protected":
+                        filterFields = fields.Where(f => f.IsFamily).ToArray();
+                        break;
+                    case "private":
+                        filterFields = fields.Where(f => f.IsPrivate).ToArray();
+                        break;
+                    case "public":
+                        filterFields = fields.Where(f => f.IsPublic).ToArray();
+                        break;
+                    case "all":
+                        filterFields = fields;
+                        break;
+                    default:
+                        filterFields = null;
+                        break;
+                }
+
+                string[] result = filterFields.Select(f => $"{f.Attributes.ToString().ToLower()} {f.FieldType.Name} {f.Name}").ToArray();
+                Console.WriteLine(string.Join(Environment.NewLine, result).Replace("family", "protected"));
+                 */
+                #endregion
+
+                filter[input]()
+                    .Select(f => $"{f.Attributes.ToString().ToLower()} {f.FieldType.Name} {f.Name}")
+                    .ToList()
+                    .ForEach(f => Console.WriteLine(f.Replace("family", "protected")));
+            }
         }
     }
 }
---
+++ OOP Advanced/10_Reflection_Exercises/Exercises_02/BlackBoxIntegerTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Reflection_Exercises
+{
+    using System;
+
+    class BlackBoxIntegerTests
+    {
+        static void Main()
+        {
+            //TODO write your solution of Problem 2. Black Box Integer here
+        }
+    }
+}
