cc708d07b85677ce506622140f37226d73f8d45a
kolio-PC\kolio <kulio_kolef@bav.bg>
2017-08-05 22:01:58 +0300

    Unit test

---
+++ OOP Advanced/11_Unit_Testing_Lab.Test/AxeTest.cs
@@ -0,0 +1,65 @@
+﻿namespace Unit_Testing_Lab.Test
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class AxeTest
+    {
+        private const int AxeAttack = 10;
+        private const int AxeDurability = 10;
+        private const int DummyHealth = 20;
+        private const int DummyExperience = 20;
+
+        private Axe axe;
+        private Dummy dummy;
+
+        [SetUp]
+        public void TestInit()
+        {
+            //Arange
+            this.axe = new Axe(AxeAttack, AxeDurability);
+            this.dummy = new Dummy(DummyHealth, DummyExperience);
+        }
+
+        [Test]
+        public void AxeLosesDurability()
+        {
+            //Arange
+            //Axe axe = new Axe(attack, durability);
+            //Dummy dummy = new Dummy(10, 10);
+
+            //Act
+            axe.Attack(dummy);
+
+            //Assert
+            //Assert.AreEqual(9, axe.DurabilityPoints);
+            Assert.IsTrue(axe.DurabilityPoints == 9);
+        }
+
+        [Test]
+        public void BrokenAxe()
+        {
+            //Arange
+            //Axe axe = new Axe(1, 1);
+            //Dummy dummy = new Dummy(10, 10);
+            this.axe = new Axe(1, 1);
+
+
+            //Act
+            axe.Attack(dummy);
+
+            //Assert
+
+            //Assert.Throws<InvalidOperationException>(
+            //    () => axe.Attack(dummy)
+            //    );
+
+            InvalidOperationException ex = Assert.Throws<InvalidOperationException>(
+                () => axe.Attack(dummy)
+                );
+
+            Assert.AreEqual("Axe is broken.", ex.Message);
+        }
+    }
+}
