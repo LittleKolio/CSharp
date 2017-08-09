6eef20b245cdf6442f7430433b0001b51dcb6174
kolio-PC\kolio <kulio_kolef@bav.bg>
2017-08-09 07:17:31 +0300

    SOLID

---
+++ OOP Advanced/13_SOLID_Lab/Lab_04/Lab_04_Recharge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Lab.Lab_04
+{
+    class Lab_04_Recharge
+    {
+        static void Main()
+        {
+            //IRechargeable rob = new RobotAdapter("ideeee", 45);
+            //Employee emp = new Employee("emploooo");
+            //RechargeStation rechStat = new RechargeStation();
+            //rechStat.Recharge();
+        }
+    }
+}
---
+++ OOP Advanced/13_SOLID_Lab/Lab_04/RobotAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Lab.Lab_04
+{
+    public class RobotAdapter : IRechargeable
+    {
+        private Robot rob;
+
+        public RobotAdapter(string id, int capacity)
+        {
+            this.rob = new Robot(id, capacity);
+        }
+        public void Recharge()
+        {
+            this.rob.Recharge();
+        }
+    }
+}
