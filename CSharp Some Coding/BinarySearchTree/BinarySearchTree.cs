6f44ac65b21a776cc729f864c408f047ff64c6f7
kolio-PC\kolio <kulio_kolef@bav.bg>
2017-12-15 13:21:11 +0200

    Practise project

---
+++ CSharp Some Coding/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,59 @@
+﻿namespace BinaryTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class BinarySearchTree<T>
+        where T : IComparable<T>
+    {
+        public class BinaryTreeNode<T>
+            : IComparable<BinaryTreeNode<T>>
+            where T : IComparable<T>
+        {
+            private T value;
+            private BinaryTreeNode<T> parent;
+            private BinaryTreeNode<T> leftChild;
+            private BinaryTreeNode<T> rightChild;
+
+            public BinaryTreeNode(T value)
+            {
+                this.value = value;
+                this.leftChild = null;
+                this.rightChild = null;
+                this.parent = null;
+            }
+
+            public override string ToString()
+            {
+                return this.value.ToString();
+            }
+
+            public override int GetHashCode()
+            {
+                return this.value.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                BinaryTreeNode<T> other = (BinaryTreeNode<T>)obj;
+                return this.CompareTo(other) == 0;
+            }
+
+            public int CompareTo(BinaryTreeNode<T> other)
+            {
+                return this.value.CompareTo(other.value);
+            }
+
+        }
+
+        private BinaryTreeNode<T> root;
+
+        public BinarySearchTree()
+        {
+            this.root = null;
+        }
+    }
+}
