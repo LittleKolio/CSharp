fa72b7147afb42f6407315a7d039734e6b7bc175
LittleKolio_PC\LittleKolio <kulio_kolef@abv.bg>
2018-01-12 09:37:28 +0200

    Data structures

---
+++ Data Structures/03_Linear_Data_Structures_Exercise/Exercises_01_Sum_and_Average/Startup.cs
@@ -0,0 +1,51 @@
+﻿namespace Exercises_01_Sum_and_Average
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<int> list = new List<int>();
+
+            string result = "Sum={0}; Average={1:F2}";
+            int sum = 0;
+            double avr = 0.0;
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                string[] tokens = input.Split(new char[] { ' ' },
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                int temp;
+                foreach (string item in tokens)
+                {
+                    if (int.TryParse(item, out temp))
+                    {
+                        list.Add(temp);
+                    }
+                    else
+                    {
+                        list.Add(default(int));
+                    }
+                }
+
+                sum = list.Sum();
+                avr = list.Average();
+            }
+            //else
+            //{
+            //    throw new ArgumentNullException(
+            //        "Nothing is passed to the console");
+            //}
+
+            Console.WriteLine(result, sum, avr);
+        }
+    }
+}
---
+++ Data Structures/03_Linear_Data_Structures_Exercise/Exercises_02_Sort_Words/Startup.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercises_02_Sort_Words
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
