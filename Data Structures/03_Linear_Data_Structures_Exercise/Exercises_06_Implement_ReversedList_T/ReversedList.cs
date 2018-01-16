474f349db925c2d9f25c1758b589f41105c63b6a
LittleKolio_PC\LittleKolio <kulio_kolef@abv.bg>
2018-01-16 09:22:15 +0200

    Data structures

---
+++ Data Structures/03_Linear_Data_Structures_Exercise/Exercises_06_Implement_ReversedList_T/ReversedList.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercises_06_Implement_ReversedList_T
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ReversedList<T> : IEnumerable<T>
+    {
+        private 
+        public static void Main(string[] args)
+        {
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
