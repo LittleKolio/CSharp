dc0afd9c9414185c9d808fee2c0b8868a3afdfb9
LittleKolio_PC\LittleKolio <kulio_kolef@abv.bg>
2018-02-16 07:47:38 +0200

    C# Fundamentals

---
+++ OOP Basics/02_Defining_Classes_Exercises/Classes/FamilyMember.cs
@@ -0,0 +1,25 @@
+﻿namespace Defining_Classes_Exercises.Classes
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class FamilyMember
+    {
+        public string Name { get; set; }
+        public string Birthday { get; set; }
+
+        public List<FamilyMember> Parents { get; set; }
+        public List<FamilyMember> Children { get; set; }
+
+        public FamilyMember()
+        {
+            this.Parents = new List<FamilyMember>();
+            this.Children = new List<FamilyMember>();
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Birthday}";
+        }
+    }
+}
---
+++ OOP Basics/02_Defining_Classes_Exercises/StartUp/Exercises_13_Family_Tree.cs
@@ -0,0 +1,88 @@
+﻿namespace Defining_Classes_Exercises.StartUp
+{
+    using Classes;
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Exercises_13_Family_Tree
+    {
+        public static List<FamilyMember> members;
+        public static string format = "dd/MM/yyyy";
+        public static void Main()
+        {
+            members = new List<FamilyMember>();
+
+            string search = Console.ReadLine();
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] tokens = input
+                    .Split(new char[] { '-' },
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                FamilyMember member1;
+                FamilyMember member2;
+
+                if (tokens.Length > 1)
+                {
+                    Parser(tokens[0], out member1);
+                    Parser(tokens[1], out member2);
+
+                    member1.Children.Add(member2);
+                    member2.Parents.Add(member1);
+                }
+                else
+                {
+                    string[] info = tokens[0].Split(new char[] { ' ' },
+                        StringSplitOptions.RemoveEmptyEntries);
+                    Parser(info[0] + " " + info[1], out member1);
+                    Parser(info[2], out member1);
+                }
+            }
+
+            FamilyMember member = members.FirstOrDefault(
+                m => m.Name == search);
+            Console.WriteLine(member);
+            Console.WriteLine("Parents:");
+            member.Parents.ForEach(p => Console.WriteLine(p));
+            Console.WriteLine("Children:");
+            member.Children.ForEach(c => Console.WriteLine(c));
+
+        }
+
+        private static void Parser(string input, out FamilyMember member)
+        {
+            member = members.FirstOrDefault(
+                m => m.Birthday == input || m.Name == input);
+
+            if (member == null)
+            {
+                member = new FamilyMember();
+                members.Add(member);
+            }
+
+            if (input.Any(Char.IsDigit) && member.Birthday == null)
+            {
+                member.Birthday = input;
+            }
+            else if (member.Name == null)
+            {
+                member.Name = input;
+            }
+        }
+
+        //private static bool Parser(string input, out DateTime birthday)
+        //{
+        //    return DateTime.TryParseExact(
+        //        input, "dd/MM/yyyy", 
+        //        CultureInfo.InvariantCulture,
+        //        DateTimeStyles.None,
+        //        out birthday);
+        //}
+    }
+}
