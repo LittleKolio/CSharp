9179d98c426fb54861a580610e2978d98c67b8c7
kolio-PC\kolio <kulio_kolef@bav.bg>
2018-02-27 23:49:53 +0200

    C# OOP Basics

---
+++ OOP Basics/new_08_Interfaces_And_Abstraction_Exercises/Exercise_01_Define_Interface_IPerson/Citizen.cs
@@ -0,0 +1,11 @@
+﻿public class Citizen : IPerson
+{
+    public int Age { get; set; }
+    public string Name { get; set; }
+
+    public Citizen(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
