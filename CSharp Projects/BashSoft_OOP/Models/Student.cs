30494b0fe2007840872d971ae77b83f6513f5ab8
LittleKolio_PC\LittleKolio <kulio_kolef@abv.bg>
2018-02-27 06:47:28 +0200

    C# BashSoft_OOP

---
+++ CSharp Projects/BashSoft_OOP/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace BashSoft_OOP
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    public class Student
+    {
+        private string name;
+        private List<int> grade;
+        public string Name { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
