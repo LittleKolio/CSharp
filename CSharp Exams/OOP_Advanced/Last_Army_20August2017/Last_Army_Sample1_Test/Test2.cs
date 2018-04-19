b76d6c3e4b38a31596a19d079cfb9c6a9df24c73
LittleKolio <kulio_kolef@abv.bg>
2018-04-19 14:05:29 +0200

    C# OOP Advanced

---
+++ CSharp Exams/OOP_Advanced/Last_Army_20August2017/Last_Army_Sample1_Test/Test2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class Test2
+{
+    private IArmy army;
+    private IAmmunitionFactory factory;
+    private IWareHouse wareHouse;
+    private IMissionController controller;
+
+    [SetUp]
+    public void Initialization()
+    {
+        this.army = new Army();
+        this.factory = new AmmunitionFactory();
+        this.wareHouse = new WareHouse(this.factory);
+        this.controller = new MissionController(this.army, this.wareHouse);
+    }
+
+    [Test]
+    public void PerformMission_EasyMission()
+    {
+        IMission mission = new Easy(10);
+
+        Assert.That(controller.PerformMission(mission), 
+            Is.EqualTo("Mission on hold - Suppression of civil rebellion"));
+    }
+
+    [Test]
+    public void PerformMission_MediumMission()
+    {
+        IMission mission = new Medium(10);
+
+        Assert.That(controller.PerformMission(mission),
+            Is.EqualTo("Mission on hold - Capturing dangerous criminals"));
+    }
+
+    [Test]
+    public void PerformMission_HardMission()
+    {
+        IMission mission = new Hard(10);
+
+        Assert.That(controller.PerformMission(mission),
+            Is.EqualTo("Mission on hold - Disposal of terrorists"));
+    }
+}
